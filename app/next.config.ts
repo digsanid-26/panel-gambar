9b5d0e86b12c431394637ee8541f82bac32c9131
mangsinaga <mangsinaga@gmail.com>
2026-04-24 11:18:25 +0700

    fix(deploy): remove output standalone, incompatible with next start

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost"],
-  output: "standalone",
   images: {
     remotePatterns: [
       {
