6c640fb5945d6661e6060e3d043c324a980a5bd8
mangsinaga <mangsinaga@gmail.com>
2026-05-18 19:00:22 +0700

    fix: update setContent call to use options object for TipTap v2+

--- app/src/components/ui/rich-text-editor.tsx
+++ app/src/components/ui/rich-text-editor.tsx
@@ -57,7 +57,7 @@ export function RichTextEditor({ value, onChange, placeholder = "Tulis konten di
 
   useEffect(() => {
     if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value, false);
+      editor.commands.setContent(value, { emitUpdate: false });
     }
   }, [value, editor]);
 
